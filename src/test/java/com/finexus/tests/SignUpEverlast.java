438c2f37712d2d5c988322afad10ddde9724608a
daskhatri <blubd.softtech@gmail.com>
2020-01-20 17:08:49 +0500

    Commit to add the file: SignUpEverlast.java

---
+++ src/test/java/com/finexus/tests/SignUpEverlast.java
@@ -0,0 +1,89 @@
+package com.finexus.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SignUpEverlast {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testSignUpEverlast() throws Exception {
+    driver.get("https://www.everlast.com/");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Search'])[3]/following::span[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Login'])[2]/following::a[1]")).click();
+    driver.findElement(By.id("firstname")).click();
+    driver.findElement(By.id("firstname")).clear();
+    driver.findElement(By.id("firstname")).sendKeys("Das");
+    driver.findElement(By.id("lastname")).clear();
+    driver.findElement(By.id("lastname")).sendKeys("Khatri");
+    driver.findElement(By.id("email_address")).click();
+    driver.findElement(By.id("email_address")).clear();
+    driver.findElement(By.id("email_address")).sendKeys("blubd.softtech@gmail.com");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("everlast#123");
+    driver.findElement(By.id("password-confirmation")).click();
+    driver.findElement(By.id("password-confirmation")).clear();
+    driver.findElement(By.id("password-confirmation")).sendKeys("everlast#123");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Confirm Password'])[1]/following::span[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Contact Information'])[1]/following::p[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Email Lists'])[1]/following::span[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Sign up for Everlast Email to receive future deals, new product alerts and more.'])[1]/following::button[1]")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
