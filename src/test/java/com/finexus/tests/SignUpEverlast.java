c4c7cc71975641b9fb48a6d389aa347c6fb94e8d
daskhatri <blubd.softtech@gmail.com>
2020-01-20 17:41:34 +0500

    Commit to add the file: SignUpEverlast.java

--- src/test/java/com/finexus/tests/SignUpEverlast.java
+++ src/test/java/com/finexus/tests/SignUpEverlast.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class SignUpEverlast {
@@ -16,7 +16,7 @@ public class SignUpEverlast {
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
+    WebDriverManager.chromedriver().setup(); driver = new ChromeDriver()();
     baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
